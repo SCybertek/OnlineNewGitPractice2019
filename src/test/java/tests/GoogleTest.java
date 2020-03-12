1995e069db378be64682adfa1503fa3d9e95076f
SCybertek <snuryyeva@gmail.com>
2020-03-11 21:57:05 -0400

    added new user

--- src/test/java/tests/GoogleTest.java
+++ src/test/java/tests/GoogleTest.java
@@ -1,6 +1,11 @@
 package tests;
 
 public class GoogleTest {
-//this is a comment 
+//this is a comment
+
+
+    //pull ==> from remote to working directory
+    //fetch ==> from remote repository to local repository
+    //merge ==> from local repository to working directory
 
 }

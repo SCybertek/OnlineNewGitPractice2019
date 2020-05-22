b369a43460a65ffff5a7f15d480956a27c8d7cfe
SCybertek <snuryyeva@gmail.com>
2020-05-22 17:57:18 -0400

    added comment

--- src/test/java/tests/RedClass.java
+++ src/test/java/tests/RedClass.java
@@ -5,5 +5,6 @@ public class RedClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("adding ");
+        //we committed but didnot push ..and our file is alreday turned white from green
     }
 }
